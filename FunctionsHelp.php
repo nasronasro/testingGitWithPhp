dda8105923d112c8d8dd47116863380bf976bf8e
nasronasro <ahfir49481@gmail.com>
2024-06-16 23:42:46 +0100

    Refactoring the code adding functionHelp file

---
+++ FunctionsHelp.php
@@ -0,0 +1,6 @@
+<?php 
+    function headingTo($location){
+        header("location:$location");
+        die();
+    }
+?>
--- login.php
+++ login.php
@@ -1,27 +1,28 @@
 <?php session_start();
-if(!isset($_POST["username"])){
-    header("location:login.html");
-    die();
-}
+//functions that helps with the programe such as headingTo()
+include "functionsHelp.php";
+if(!isset($_POST["username"]))
+    headingTo("login.html");
+
+if($username == "" || $pass == "")
+    headingTo("login.html");
+//declaration for variables
 $username = $_POST["username"];
 $pass = $_POST["password"];
-
-if($username == "" || $pass == ""){
-    header("location:login.html");
-    echo "username or password is empty";
-    die();
-}
+//connection avec database
 require_once "connection.php";
+//getting current user
 $res = $con->prepare("select username, password from accounts where username = :username");
 $res->bindParam(":username",$username);
 $res->execute();
+//getting the current user if it exist else redirect to login form
 if($data = $res->fetch(PDO::FETCH_ASSOC)){
-    $_SESSION["username"] = $username;
-    header("location:main.html");
-    die();
-}else{
-    header("location:login.html");
-    echo"no user with this name";
-    die();
+    //if the verification correct we go to the main window
+    if($username == $data["username"] && $pass == $data["password"]){
+        $_SESSION["username"] = $username;
+        headingTo("main.html");
+    }else
+        headingTo("login.html");
+    
 }
 ?>
